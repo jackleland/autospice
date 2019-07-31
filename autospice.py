c8f573343172765a2ee5bfd3ca2049ab36f4aca4
Jack Leland <j.leland@liverpool.ac.uk>
2019-07-31 17:25:10 +0100

    Fix Parameter Scan Input File Creation Bug
    The input file added to a parameter directory was using the input file's
    parent path for creation. This has now been changed to the proper
    directory.

--- autospice.py
+++ autospice.py
@@ -159,7 +159,7 @@ def submit_job(config_file, dryrun_fl=False):
                 if not dryrun_fl:
                     os.mkdir(output_dir)
 
-                input_file = input_file_base.parent / param_dir / f'{param_dir}_{input_file_base.name}'
+                input_file = output_dir / f'{param_dir}_{input_file_base.name}'
                 inp_parser[scan_param['section']][scan_param['parameter']] = param_value
                 if not dryrun_fl:
                     with open(input_file, 'w') as f:
